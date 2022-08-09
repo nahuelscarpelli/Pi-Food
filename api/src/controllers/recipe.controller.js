5c76513868de4fb13d439c7f3b4f0f7de1ee137a
Tu usuario de GitHub aca <Tu email aca>
2022-08-09 11:37:19 -0300

    9-8 2

---
+++ api/src/controllers/recipe.controller.js
@@ -0,0 +1,103 @@
+require("dotenv").config();
+const { Recipe, Diet } = require("../db.js");
+const axios = require("axios");
+//const { Op } = require("sequelize");
+const { YOUR_API_KEY } = process.env;
+
+// GET RECIPES
+
+function getRecipes(req, res, next) {
+  const nameQuery = req.query.name;
+  var remoteRecipes = [];
+  var localRecipes = [];
+  if (nameQuery) {
+    axios
+      .get(
+        `https://api.spoonacular.com/recipes/complexSearch?apiKey=${YOUR_API_KEY}&addRecipeInformation=true&query=${nameQuery}&number=100`
+      )
+      .then((apiResponse) => {
+        remoteRecipes = apiResponse.data.results.filter((recipe) => {
+          return recipe.title.toLowerCase().includes(nameQuery);
+        });
+        return Recipe.findAll({ include: [Diet] });
+      })
+      .then((localResponse) => {
+        localRecipes = localResponse.filter((recipe) => {
+          return recipe.title.toLowerCase().includes(nameQuery);
+        });
+        return res.json([...localRecipes, ...remoteRecipes].slice(0, 9));
+      })
+      .catch((error) => next(error));
+  } else {
+    axios
+      .get(
+        `https://api.spoonacular.com/recipes/complexSearch?apiKey=${YOUR_API_KEY}&addRecipeInformation=true&number=100`
+      )
+      .then((apiResponse) => {
+        remoteRecipes = apiResponse.data.results;
+        return Recipe.findAll({ include: [Diet] });
+      })
+      .then((localResponse) => {
+        return res.json([...localResponse, ...remoteRecipes]);
+      })
+      .catch((error) => next(error));
+  }
+}
+
+// GET RECIPES BY ID
+
+function getRecipeById(req, res, next) {
+  const id = req.params.idReceta;
+  if (id.includes("-")) {
+    Recipe.findByPk(id, { include: Diet }).then((recipe) => {
+      return res.json(recipe);
+    });
+  } else {
+    axios
+      .get(
+        `https://api.spoonacular.com/recipes/${id}/information?apiKey=${YOUR_API_KEY}`
+      )
+      .then((response) => {
+        return res.json({
+          title: response.data.title,
+          image: response.data.image,
+          dishTypes: response.data.dishTypes,
+          diets: response.data.diets,
+          summary: response.data.summary,
+          score: response.data.spoonacularScore,
+          healthScore: response.data.healthScore,
+          instructions: response.data.instructions,
+        });
+      })
+      .catch((error) => next(error));
+  }
+}
+
+// POST CREATE RECIPE
+
+function createRecipe(req, res, next) {
+  const { title, summary, score, healthScore, instructions, diets } = req.body;
+  Recipe.create({
+    title,
+    image: "",
+    summary,
+    score: parseFloat(score),
+    healthScore: parseFloat(healthScore),
+    instructions,
+  })
+    .then((recipeCreated) => {
+      return recipeCreated.setDiets(diets);
+    })
+    .then((newRecipe) => {
+      return res.json({
+        message: "Recipe created successfully",
+      });
+    })
+    .catch((error) => next(error));
+}
+
+module.exports = {
+  getRecipes,
+  getRecipeById,
+  createRecipe,
+};
