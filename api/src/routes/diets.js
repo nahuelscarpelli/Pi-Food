8ad24eb640e56479561e5ee8b135ed8970e02a65
Tu usuario de GitHub aca <Tu email aca>
2022-08-09 11:21:36 -0300

    9/8

--- api/src/routes/diets.js
+++ api/src/routes/diets.js
@@ -1,7 +1,7 @@
-const express = require('express')
-const router = express.Router()
-const { Diet } = require('../db');
+const { Router } = require("express");
+const router = Router();
+const { getDiets } = require("../controllers/diet.controller");
 
+router.get("/types", getDiets);
 
-
-module.exports = router
+module.exports = router;
